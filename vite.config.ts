35ee41c925294daea5956e1122f1f61c49761bd9
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2024-06-25 13:44:14 +0700

    add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   ssr: {
     external: ["react-router-dom"],
     noExternal: ["react-helmet-async"],
